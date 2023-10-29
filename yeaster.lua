4b98a2e825ca1372d5f43275547beccdf3bb617b
opera-joakimd <joakimd@opera.com>
2023-10-29 19:18:04 +0100

    Add yeaster

---
+++ yeaster.lua
@@ -0,0 +1,37 @@
+-- setup:
+-- output chest below
+-- redstone trigger for sugar dropper to the right
+local turtle = require("turtle")
+
+function DropSugar()
+    redstone.setOutput("right", true)
+    os.sleep(1 / 20)
+    redstone.setOutput("right", false)
+end
+
+function MakeYeast()
+    -- place water
+    turtle.place()
+    DropSugar()
+    while true do
+        local _, liquid = turtle.inspect()
+        if liquid and liquid.name == "pneumaticcraft:yeast_culture" then
+            -- scoop yeast
+            turtle.place()
+            turtle.dropDown()
+            return
+        end
+    end
+end
+
+function Main()
+    local item = turtle.getItemDetail(1)
+
+    if item and item.name == "minecraft:water_bucket" then
+        MakeYeast()
+    end
+end
+
+while true do
+    Main()
+end
